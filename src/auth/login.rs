f208612a38a965af2dafc57f10fe4d4eb03e2494
stella <st3lla_programming@protonmail.com>
2023-06-15 18:51:14 +1200

    Added basic login and register skeleton

--- src/auth/login.rs
+++ src/auth/login.rs
@@ -1,4 +1,21 @@
-#[get("/login")]
-pub fn login() -> &'static str {
-    "login"
+use rocket::http::Status;
+use rocket::serde::json::Json;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(FromForm, Serialize, Deserialize)]
+pub struct Login {
+    name: String,
+    username: String,
+}
+
+#[post("/login", format="json", data="<user>")]
+pub fn login(user: Json<Login>) -> Status{
+
+    //Print the user data
+    println!("Name: {}", user.name);
+    println!("Username: {}", user.username);
+
+    //Return a success status
+    Status::Ok
 }
--- src/auth/register.rs
+++ src/auth/register.rs
@@ -1,4 +1,23 @@
-#[get("/register")]
-pub fn register() -> &'static str {
-    "register"
+use rocket::http::Status;
+use rocket::serde::json::Json;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(FromForm, Serialize, Deserialize)]
+pub struct Register {
+    name: String,
+    email: String,
+    username: String,
+}
+
+#[post("/register", format="json", data="<user>")]
+pub fn register(user: Json<Register>) -> Status{
+
+    //Print the user data
+    println!("Name: {}", user.name);
+    println!("Email: {}", user.email);
+    println!("Username: {}", user.username);
+
+    //Return a success status
+    Status::Ok
 }
